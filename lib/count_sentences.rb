3b967ff770c75f99e6b3b8e82e9899d3b920c534
Victor Williams <victor.williams1030@gmail.com>
2020-05-21 18:57:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,6 +28,6 @@ class String
 
   def count_sentences
     self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
-    # thank you StackOverflow. Here's the breakdown: 
+    # thank you StackOverflow. Here's the breakdown: [^]
   end
 end
